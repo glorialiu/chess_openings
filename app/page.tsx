f5d7e85170567de2592f2bbdc1e99d525d496a1f
Gloria Liu <gloriafire96@gmail.com>
2023-12-21 15:28:16 -1000

    read openings from eco json

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,52 @@
 "use client";
 
 import { useState } from "react";
+import openingsJson from "../eco.json";
 import styles from "./page.module.css";
+
+interface opening {
+  name: string;
+  moves: string;
+}
 
 export default function Home() {
   const [index, setIndex] = useState(0);
 
-  const openings = [
-    "the english",
-    "the sicilian",
-    "dutch",
-    "queens gambit",
-    "halloween",
-  ];
-
-  // TODO prevent getting the same opening twice in a row
   function newOpening() {
-    setIndex(getRandomInt(openings.length));
+    setIndex(getRandomInt(openingsJson.length));
   }
   function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
+    var newRandom = Math.floor(Math.random() * max);
+    while (newRandom == index) {
+      newRandom = Math.floor(Math.random() * max);
+    }
+    return newRandom;
   }
 
-  function renderOpening() {
-    return openings[index];
+  function renderOpeningName() {
+    return openingsJson[index].name;
+  }
+
+  function renderOpeningMoves() {
+    return openingsJson[index].moves;
   }
 
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <div className={styles.title}>
         <p>Opening Randomizer</p>
       </div>
-      <div className={styles.description}>{renderOpening()}</div>
       <div className={styles.description}>
-        <button onClick={newOpening}>New Opening!</button>;
+        {renderOpeningMoves()}
+        <br />
+        <br />
+        {renderOpeningName()}
+      </div>
+      <div>
+        <button className={styles.newOpeningButton} onClick={newOpening}>
+          New Opening!
+        </button>
+        ;
       </div>
     </main>
   );
